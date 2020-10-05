7ec69a9171604e745e92f7142dbcb244bc55a074
Pradyun B <59351712+pbprimeplay@users.noreply.github.com>
2020-10-05 10:35:45 +0530

    Update main.py

--- Files/main.py
+++ Files/main.py
@@ -3,7 +3,7 @@ from tkinter import *
 from tkinter import ttk
 import tkinter.font as tkFont
 
-items = []
+currencyNames = []
 #Reading the lines of the data file
 with open("Assets/conversionData.txt") as f:
 	lines = f.readlines()
@@ -16,7 +16,7 @@ for line in lines:
 
 #Appending names of currencies to a list
 for names in currencyDict.keys():
-	items.append(names)
+	currencyNames.append(names)
 
 #Creating and formatting the tkinter GUI
 root = Tk()
@@ -26,7 +26,7 @@ root.iconbitmap("Assets/ProgramIcon.ico")
 root.resizable(False, False)
 
 #Initialising answer output label beforhand
-answer = Label(root)
+answer = Label(root, fg="green")
 
 #String Variables for the ttk combobox dropdown element
 variable1 = StringVar(root)
@@ -36,36 +36,39 @@ variable2 = StringVar(root)
 variable2.set("Select Currency")
 
 #Creating a function which converts values
-def convert(conval1, conval2):
+def convert(conval1, conval2, amount):
 	#Formulating the final value
-	converted = float(moneyVal.get())/float(currencyDict[conval1]) * float(currencyDict[conval2])
+	converted = float(amount)/float(currencyDict[conval1]) * float(currencyDict[conval2])
 	#Rounding it upto only 2 decimal places
 	conv = f"{converted:.2f}"
 	formatted = conv+' '+conval2 + 's'
 
 	answer.place_forget()
 
-	answer.config(text=formatted, fg="green")
+	answer.config(text=formatted)
 	answer.place(x=180, y=155, anchor="center")
 
 #Title labels
 titleText = Label(root, text='Currency Converter', bg='white', fg="black")
 titleText.place(x=120, y=5)
+
 #Extra fonts
 fontStyle1 = tkFont.Font(titleText, titleText.cget("font"))
 fontStyle1.configure(underline = True)
+fontStyle2 = tkFont.Font(size=30)
+
+#Adding Font Styles to title
 titleText.config(font=fontStyle1)
 
 #First currency dropdown combobox
-currency1 = ttk.Combobox(root, textvariable=variable1, values=items)
+currency1 = ttk.Combobox(root, textvariable=variable1, values=currencyNames)
 currency1.place(x=10, y=40)
 
-fontStyle2 = tkFont.Font(size=30)
 to = Label(root, text="↠", font=fontStyle2, fg='red')
 to.place(x=160, y=26)
 
 #Second currency dropdown combobox
-currency2 = ttk.Combobox(root, textvariable=variable2, values=items)
+currency2 = ttk.Combobox(root, textvariable=variable2, values=currencyNames)
 currency2.place(x=190, y=40)
 
 #Amount entry input
@@ -73,7 +76,7 @@ moneyVal = Entry(root)
 moneyVal.place(x=110, y=80)
 
 #Enter/Submission button
-enter = Button(root, text="Enter", command=lambda: convert(currency1.get(), currency2.get()))
+enter = Button(root, text="Enter", command=lambda: convert(currency1.get(), currency2.get(), moneyVal.get())
 enter.place(x=150, y=110)
 
 #Mainloop
